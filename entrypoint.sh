c5b976f7af0214a18ca21e422592eeeb243b2721
Picassio <tuanle@Picassios-MacBook-Pro.local>
2020-05-03 21:01:31 +0700

    edit user create command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
+set -e
 
-groupadd -g 1000 ftpgroup && useradd -u 1000 -G ftpgroup -d /home/ftpuser -s /sbin/nologin ftpuser
+groupadd -g 1000 ftpgroup && useradd -u 1000 -g 1000 -d /home/ftpuser -s /sbin/nologin ftpuser
 chown -R ftpuser:ftpgroup /home/ftpuser
 [[ -f /opt/purefptd/etc/pureftpd.passwd ]] && /opt/purefptd/bin/pure-pw mkdb || touch /opt/purefptd/etc/pureftpd.passwd && /opt/purefptd/bin/pure-pw mkdb
 
